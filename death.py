9bfda7bfda67638372d9f18b51a88c1b41826435
Jeffrey Woods <jeffreytwoods2@icloud.com>
2022-02-06 17:59:11 -0700

    Restructure files to solve import problems

---
+++ death.py
@@ -0,0 +1,8 @@
+from textwrap import dedent
+
+class Death(object):
+
+    def start(self):
+        print(self.fredbearAscii)
+        print("\nYou died.")
+        exit(1)
--- main.py
+++ main.py
@@ -1,7 +1,54 @@
-import map as Map
-import engine as Engine
+import sys
+import time
+import random
+from sys import exit
 
-start_scene = Map('midnight')
+deathArt = open("death_art.txt")
+
+class Death(object):
+
+    def start(self):
+        print(deathArt.read())
+        deathArt.close()
+        print("\nYou died.")
+        exit(1)
+
+class Engine(object):
+
+    def __init__(self, scene_map):
+        self.scene_map = scene_map
+
+    def play(self):
+        current_scene = self.scene_map.opening_scene()
+        last_scene = self.scene_map.next_scene('six-am')
+
+        while current_scene != last_scene:
+            next_scene_name = current_scene.start()
+            current_scene = self.scene_map.next_scene(next_scene_name)
+
+        current_scene.start()
+
+class Map(object):
+    scenes = {
+        # 'midnight': Midnight(),
+        # 'hallway_empty': HallwayEmpty(),
+        # 'fredbear_far': FredbearFar(),
+        # 'fredbear_near': FredbearNear(),
+        # 'fredbear_at_desk': FredbearAtDesk(),
+        'death': Death(),
+        # 'six-am': SixAm(),
+    }
+
+    def __init__(self, start_scene):
+        self.start_scene = start_scene
+
+    def next_scene(self, scene_name):
+        get_next_scene = Map.scenes.get(scene_name)
+        return get_next_scene
+
+    def opening_scene(self):
+        return self.next_scene(self.start_scene)
+
+start_scene = Map('death')
 start_engine = Engine(start_scene)
 start_engine.play()
-
